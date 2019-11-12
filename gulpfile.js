9213a6a2ff9f0904ed33dffc5d5edf42cf73edde
Roman Yakovlev <tensegrity666@eicloud.com>
2019-11-12 23:45:29 +0300

    Настраивает галп

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,8 @@
 
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass");
-const csso = require("gulp-csso");
-const htmlmin = require("gulp-htmlmin");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
 const plumber = require("gulp-plumber");
 const beeper = require("beeper");
-// const uglify = require("gulp-uglify");
-const rename = require("gulp-rename");
-// const posthtml = require("gulp-posthtml");
-const stylelint = require("gulp-stylelint");
 const browserSync = require("browser-sync").create();
 const reload = browserSync.reload;
 
@@ -24,23 +16,11 @@ function serve() {
 };
 
 function style() {
-  return src("source/sass/*.scss")
+  return src("source/sass/main.scss")
     .pipe(plumber(errorHandler))
-    .pipe(sass({
-      includePaths: require('node-normalize-scss').includePaths
-    }))
-    .pipe(postcss([autoprefixer()]))
-    .pipe(csso())
-    .pipe(rename("style.min.css"))
+    .pipe(sass())
     .pipe(dest("source/css"))
     .pipe(browserSync.stream());
-};
-
-function lint() {
-  return src("source/sass/**/*.scss")
-    .pipe(stylelint({
-      reporters: [{ formatter: 'string', console: true }]
-  }))
 };
 
 function errorHandler(error) {
@@ -48,31 +28,5 @@ function errorHandler(error) {
   return true;
 }
 
-// function html() {
-//   return gulp.src("./source/*.html")
-//     .pipe(posthtml([
-//       include()
-//     ]))
-//     .pipe(gulp.dest("./source"));
-// };
-
-function htmlminify() {
-  return src("source/*.html")
-    .pipe(htmlmin())
-    .pipe(dest("source"));
-};
-
-// function jsminify() {
-//   return gulp.src("/source/js/script.js")
-//     .pipe(uglify())
-//     .pipe(rename("script.min.js"))
-//     .pipe(gulp.dest("/source/js"))
-//     .pipe(browserSync.stream());
-// };
-
 exports.style = style;
-exports.lint = lint;
-// exports.html = html;
-exports.htmlminify = htmlminify;
-// exports.jsminify = jsminify;
 exports.serve = serve;
